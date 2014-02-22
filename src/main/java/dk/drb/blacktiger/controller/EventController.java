b241585b411ca295f455080365d774e75a2d5ec6
michael <michael@192.168.87.101>
2014-02-22 12:18:36 +0100

    Added EventController.

--- src/main/java/dk/drb/blacktiger/controller/EventController.java
+++ src/main/java/dk/drb/blacktiger/controller/EventController.java
@@ -1,6 +1,12 @@
 package dk.drb.blacktiger.controller;
 
+import dk.drb.blacktiger.model.ParticipantEvent;
+import java.util.List;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  *
@@ -9,4 +15,9 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class EventController {
     
+    @RequestMapping(value="/events", method = RequestMethod.GET)
+    @ResponseBody
+    public List<ParticipantEvent> getEvents(@RequestParam(required = false) long since, @RequestParam String[] rooms) {
+        return null;
+    }
 }
