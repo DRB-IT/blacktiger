771a07405860e245cbbd59623f694cd38401d774
Michael Krog <mic@apaq.dk>
2014-04-02 13:25:00 +0200

    MAde springsecuritycontext read values from properties file.

--- src/test/scripts/initialize.sh
+++ src/test/scripts/initialize.sh
@@ -6,6 +6,6 @@ mysql --user=root --password=root < /vagrant/src/test/sql/provision_db.sql
 
 /etc/init.d/tomcat stop
 rm -rf /opt/apache-tomcat-7.0.52/webapps/ROOT
-unzip /vagrant/target/blacktiger-2.0.0.war -d /opt/apache-tomcat-7.0.52/webapps/ROOT
+unzip /vagrant/target/blacktiger-2.0.0-SNAPSHOT.war -d /opt/apache-tomcat-7.0.52/webapps/ROOT
 cp /vagrant/src/test/config/blacktiger.properties /opt/apache-tomcat-7.0.52
 /etc/init.d/tomcat start
