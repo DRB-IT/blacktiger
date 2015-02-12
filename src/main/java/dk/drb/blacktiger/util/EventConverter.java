a96e6c3dc5f3167b92e4428e08e321f2397c3e2f
Michael Krog <mic@apaq.dk>
2015-02-12 12:01:19 +0100

    Fixed #115 again.

--- src/main/java/dk/drb/blacktiger/util/EventConverter.java
+++ src/main/java/dk/drb/blacktiger/util/EventConverter.java
@@ -9,8 +9,10 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.util.ISO8601DateFormat;
 import dk.drb.blacktiger.model.ConferenceEvent;
 import dk.drb.blacktiger.model.ConferenceStartEvent;
+import dk.drb.blacktiger.model.Participant;
 import dk.drb.blacktiger.model.ParticipantEvent;
 import dk.drb.blacktiger.model.SparseParticipantEvent;
 import java.io.IOException;
@@ -24,40 +26,65 @@ import org.springframework.messaging.converter.MappingJackson2MessageConverter;
  */
 public class EventConverter extends MappingJackson2MessageConverter {
 
+    private final JsonSerializer<ConferenceEvent> eventSerializer = new JsonSerializer<ConferenceEvent>() {
+        @Override
+        public void serialize(ConferenceEvent value, JsonGenerator jgen, SerializerProvider provider) throws IOException, JsonProcessingException {
+            jgen.writeStartObject();
+            jgen.writeStringField("roomNo", value.getRoomNo());
+
+            if (value instanceof ParticipantEvent) {
+                jgen.writeObjectField("participant", ((ParticipantEvent) value).getParticipant());
+            }
+
+            if (value instanceof SparseParticipantEvent) {
+                jgen.writeStringField("channel", ((SparseParticipantEvent) value).getChannel());
+            }
+
+            if (value instanceof ConferenceStartEvent) {
+                jgen.writeObjectField("room", ((ConferenceStartEvent) value).getRoom());
+            }
+
+            jgen.writeStringField("type", value.getType());
+            jgen.writeEndObject();
+        }
+
+        @Override
+        public Class<ConferenceEvent> handledType() {
+            return ConferenceEvent.class;
+        }
+    };
+    
+    private final JsonSerializer<Participant> participantSerializer = new JsonSerializer<Participant>() {
+        ISO8601DateFormat df = new ISO8601DateFormat();
+        @Override
+        public void serialize(Participant value, JsonGenerator jgen, SerializerProvider provider) throws IOException, JsonProcessingException {
+            String dateJoined = value.getDateJoined() == null ? null : df.format(value.getDateJoined());
+            String type = value.getType() == null ? null : value.getType().name();
+            jgen.writeStartObject();
+            jgen.writeStringField("callerId", value.getCallerId());
+            jgen.writeStringField("channel", value.getChannel());
+            jgen.writeBooleanField("muted", value.isMuted());
+            jgen.writeStringField("phoneNumber", value.getPhoneNumber());
+            jgen.writeStringField("dateJoined", dateJoined);
+            jgen.writeStringField("name", value.getName());
+            jgen.writeStringField("type", type);
+            jgen.writeBooleanField("host", value.isHost());
+            jgen.writeEndObject();
+        }
+
+        @Override
+        public Class<Participant> handledType() {
+            return Participant.class;
+        }
+    };
+
     public EventConverter() {
         SimpleModule m = new SimpleModule("EventModule");
-        m.addSerializer(new JsonSerializer<ConferenceEvent>() {
+        m.addSerializer(eventSerializer);
+        m.addSerializer(participantSerializer);
 
-            @Override
-            public void serialize(ConferenceEvent value, JsonGenerator jgen, SerializerProvider provider) throws IOException, JsonProcessingException {
-                jgen.writeStartObject();
-                jgen.writeStringField("roomNo", value.getRoomNo());
-                
-                if(value instanceof ParticipantEvent) {
-                    jgen.writeObjectField("participant", ((ParticipantEvent)value).getParticipant());
-                }
-                
-                if(value instanceof SparseParticipantEvent) {
-                    jgen.writeStringField("channel", ((SparseParticipantEvent)value).getChannel());
-                }
-                
-                if(value instanceof ConferenceStartEvent) {
-                    jgen.writeObjectField("room", ((ConferenceStartEvent)value).getRoom());
-                }
-                
-                jgen.writeStringField("type", value.getType());
-                jgen.writeEndObject();
-            }
-
-            @Override
-            public Class<ConferenceEvent> handledType() {
-                return ConferenceEvent.class;
-            }
-            
-        });
-        
         getObjectMapper().registerModule(m);
-        
+
     }
 
     @Override
@@ -69,7 +96,5 @@ public class EventConverter extends MappingJackson2MessageConverter {
     protected boolean canConvertFrom(Message<?> message, Class<?> targetClass) {
         return false;
     }
-    
-    
-     
+
 }
--- src/test/java/dk/drb/blacktiger/controller/websocket/EventControllerTest.java
+++ src/test/java/dk/drb/blacktiger/controller/websocket/EventControllerTest.java
@@ -1,5 +1,6 @@
 package dk.drb.blacktiger.controller.websocket;
 
+import com.fasterxml.jackson.databind.util.ISO8601DateFormat;
 import dk.drb.blacktiger.controller.rest.TestChannelInterceptor;
 import java.nio.charset.Charset;
 import org.junit.Before;
@@ -20,106 +21,116 @@ import dk.drb.blacktiger.model.Participant;
 import dk.drb.blacktiger.model.ParticipantLeaveEvent;
 import dk.drb.blacktiger.service.ConferenceService;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.mockito.Mockito;
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
+
 /**
  *
  * @author michael
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = { WebsocketConfig.class})
+@ContextConfiguration(classes = {WebsocketConfig.class})
 public class EventControllerTest {
+
     static {
         System.setProperty("test", "true");
     }
-    @Autowired private AbstractSubscribableChannel clientInboundChannel;
-
-	@Autowired private AbstractSubscribableChannel clientOutboundChannel;
-
-	@Autowired private AbstractSubscribableChannel brokerChannel;
-
-	private TestChannelInterceptor clientOutboundChannelInterceptor;
-
-	private TestChannelInterceptor brokerChannelInterceptor;
-        private ConferenceService service;
-        @Autowired private EventController controller;
-        private ConferenceEventListener eventListener;
-
-	@Before
-	public void setUp() throws Exception {
-
-		this.brokerChannelInterceptor = new TestChannelInterceptor(false);
-		this.clientOutboundChannelInterceptor = new TestChannelInterceptor(false);
-
-		this.brokerChannel.addInterceptor(this.brokerChannelInterceptor);
-		this.clientOutboundChannel.addInterceptor(this.clientOutboundChannelInterceptor);
-                
-                service = Mockito.mock(ConferenceService.class);
-                Mockito.doAnswer(eventListenerAnswer()).when(service).addEventListener(Mockito.any(ConferenceEventListener.class));
-                controller.setService(service);
-	}
-
-        private Answer<Void> eventListenerAnswer() {
-            return new Answer<Void>() {
-
-                @Override
-                public Void answer(InvocationOnMock invocation) throws Throwable {
-                    eventListener = (ConferenceEventListener) invocation.getArguments()[0];
-                    return null;
+    @Autowired
+    private AbstractSubscribableChannel clientInboundChannel;
+    
+    @Autowired
+    private AbstractSubscribableChannel clientOutboundChannel;
+    
+    @Autowired
+    private AbstractSubscribableChannel brokerChannel;
+    
+    private TestChannelInterceptor clientOutboundChannelInterceptor;
+    
+    private TestChannelInterceptor brokerChannelInterceptor;
+    private ConferenceService service;
+    @Autowired
+    private EventController controller;
+    private ConferenceEventListener eventListener;
+    
+    @Before
+    public void setUp() throws Exception {
+        
+        this.brokerChannelInterceptor = new TestChannelInterceptor(false);
+        this.clientOutboundChannelInterceptor = new TestChannelInterceptor(false);
+        
+        this.brokerChannel.addInterceptor(this.brokerChannelInterceptor);
+        this.clientOutboundChannel.addInterceptor(this.clientOutboundChannelInterceptor);
+        
+        service = Mockito.mock(ConferenceService.class);
+        Mockito.doAnswer(eventListenerAnswer()).when(service).addEventListener(Mockito.any(ConferenceEventListener.class));
+        controller.setService(service);
+    }
+    
+    private Answer<Void> eventListenerAnswer() {
+        return new Answer<Void>() {
+            
+            @Override
+            public Void answer(InvocationOnMock invocation) throws Throwable {
+                eventListener = (ConferenceEventListener) invocation.getArguments()[0];
+                return null;
+            }
+        };
+    }
+    
+    @Test
+    public void getEvents() throws Exception {
+        
+        StompHeaderAccessor headers = StompHeaderAccessor.create(StompCommand.SUBSCRIBE);
+        headers.setSubscriptionId("0");
+        headers.setDestination("/queue/events/H45-1234");
+        headers.setSessionId("0");
+        //Message<byte[]> message = MessageBuilder.withPayload("\"\"".getBytes()).setHeaders(headers).build();
+        Message<byte[]> message = MessageBuilder.createMessage(new byte[0], headers.getMessageHeaders());
+        
+        this.clientOutboundChannelInterceptor.setIncludedDestinations("/queue/events/H45-1234");
+        this.clientOutboundChannelInterceptor.startRecording();
+        this.clientInboundChannel.send(message);
+        
+        final List<Message> replies = new ArrayList<>();
+        Thread t = new Thread() {
+            
+            @Override
+            public void run() {
+                try {
+                    Message<?> reply = EventControllerTest.this.clientOutboundChannelInterceptor.awaitMessage(5);
+                    replies.add(reply);
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(EventControllerTest.class.getName()).log(Level.SEVERE, null, ex);
                 }
-            };
-        }
+            }
+        };
+        t.start();
+        Thread.sleep(1000);
 
-	@Test
-	public void getEvents() throws Exception {
-
-                StompHeaderAccessor headers = StompHeaderAccessor.create(StompCommand.SUBSCRIBE);
-		headers.setSubscriptionId("0");
-		headers.setDestination("/queue/events/H45-1234");
-		headers.setSessionId("0");
-		//Message<byte[]> message = MessageBuilder.withPayload("\"\"".getBytes()).setHeaders(headers).build();
-                Message<byte[]> message = MessageBuilder.createMessage(new byte[0], headers.getMessageHeaders());
-
-                this.clientOutboundChannelInterceptor.setIncludedDestinations("/queue/events/H45-1234");
-                this.clientOutboundChannelInterceptor.startRecording();
-                this.clientInboundChannel.send(message);
-
-                final List<Message> replies = new ArrayList<>();
-                Thread t = new Thread() {
-
-                    @Override
-                    public void run() {
-                        try {
-                            Message<?> reply = EventControllerTest.this.clientOutboundChannelInterceptor.awaitMessage(5);
-                            replies.add(reply);
-                        } catch (InterruptedException ex) {
-                            Logger.getLogger(EventControllerTest.class.getName()).log(Level.SEVERE, null, ex);
-                        }
-                    }
-                };
-                t.start();
-                Thread.sleep(1000);
-                
-                // Send event
-                Participant p = new Participant();
-                p.setCallerId("123");
-                eventListener.onParticipantEvent(new ParticipantLeaveEvent("H45-1234", p));
-                
-                t.join();
-                
-		Message<?> reply = replies.get(replies.size()-1);
-		assertNotNull("No reply recieved", reply);
-
-		StompHeaderAccessor replyHeaders = StompHeaderAccessor.wrap(reply);
-		assertEquals("0", replyHeaders.getSessionId());
-		assertEquals("0", replyHeaders.getSubscriptionId());
-		assertTrue("Destination should start with /events/ but was " + replyHeaders.getDestination()+ ".", replyHeaders.getDestination().startsWith("/queue/events/"));
-
-		String json = new String((byte[]) reply.getPayload(), Charset.forName("UTF-8"));
-                assertEquals("{\"roomNo\":\"H45-1234\",\"participant\":{\"callerId\":\"123\",\"channel\":null,\"muted\":false,\"phoneNumber\":null,\"dateJoined\":null,\"name\":null,\"type\":null,\"host\":false},\"type\":\"Leave\"}", json);
-	}
+        // Send event
+        Date now = new Date();
+        Participant p = new Participant();
+        p.setCallerId("123");
+        p.setDateJoined(now);
+        eventListener.onParticipantEvent(new ParticipantLeaveEvent("H45-1234", p));
+        
+        t.join();
+        
+        Message<?> reply = replies.get(replies.size() - 1);
+        assertNotNull("No reply recieved", reply);
+        
+        StompHeaderAccessor replyHeaders = StompHeaderAccessor.wrap(reply);
+        assertEquals("0", replyHeaders.getSessionId());
+        assertEquals("0", replyHeaders.getSubscriptionId());
+        assertTrue("Destination should start with /events/ but was " + replyHeaders.getDestination() + ".", replyHeaders.getDestination().startsWith("/queue/events/"));
+        
+        String json = new String((byte[]) reply.getPayload(), Charset.forName("UTF-8"));
+        ISO8601DateFormat df = new ISO8601DateFormat();
+        assertEquals("{\"roomNo\":\"H45-1234\",\"participant\":{\"callerId\":\"123\",\"channel\":null,\"muted\":false,\"phoneNumber\":null,\"dateJoined\":\"" + df.format(now) + "\",\"name\":null,\"type\":null,\"host\":false},\"type\":\"Leave\"}", json);
+    }
 }
